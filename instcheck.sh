305ebd84d1a61de773aebaf6eaf1455e3269f354
Dark-Wap <168370710+Dark-Wap@users.noreply.github.com>
2025-02-04 01:06:03 -0600

    Update instcheck.sh

--- instcheck.sh
+++ instcheck.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 clear
-echo "America/Sao_Paulo" > /etc/timezone
-ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
+echo "America/Mexico_City" > /etc/timezone
+ln -fs /usr/share/zoneinfo/America/Mexico_City /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
 echo -e "\E[44;1;37m    INSTALAR CHECKUSER CONECTA4G     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @TURBONET2023\033[1;36m"
+echo -e "                 \033[1;31mBy @VPN-NetServices\033[1;36m"
 echo ""
-echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
+echo -ne "\n\033[1;32mENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
-echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
+echo -e "\n\033[1;36mINICIANDO INSTALACIÓN \033[1;33mESPERE..."
 apt-get install figlet -y > /dev/null 2>&1
 pip3 install flask > /dev/null 2>&1
-rm /bin/chuser > /dev/null 2>&1
+rm /bin/CheckUser > /dev/null 2>&1
 sleep 5
 cd /bin || exit
-wget https://raw.githubusercontent.com/PhoenixxZ2023/CheckUSER-4G/main/chuser > /dev/null 2>&1
-wget https://raw.githubusercontent.com/PhoenixxZ2023/CheckUSER-4G/main/userscheck > /dev/null 2>&1
-chmod 777 chuser > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Dark-Wap/CheckUser-4G/refs/heads/main/CheckUser > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Dark-Wap/CheckUser-4G/refs/heads/main/userscheck > /dev/null 2>&1
+chmod 777 CheckUser > /dev/null 2>&1
 chmod 777 userscheck > /dev/null 2>&1
 clear
 mkdir /usr/lib/checkuser > /dev/null 2>&1
 cd /usr/lib/checkuser || exit
 rm checkuser.py > /dev/null 2>&1
-wget https://raw.githubusercontent.com/PhoenixxZ2023/CheckUSER-4G/main/checkuser.py > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Dark-Wap/CheckUser-4G/refs/heads/main/checkuser.py > /dev/null 2>&1
 chmod 777 checkuser.py > /dev/null 2>&1
 clear
-echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
+echo -e "        \033[1;33m • \033[1;32mINSTALACIÓN TERMINADA\033[1;33m • \033[0m"
 sleep 2
 clear
 echo ""
-echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchuser\033[0m"
+echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mCheckUser\033[0m"
 echo ""
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@TURBONET\033[0m"
+echo -e "\033[1;33mMÁS INFORMACIÓNES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@VPN-NetServices\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
