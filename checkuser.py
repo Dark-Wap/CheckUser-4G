46b2df7212410d1da0b32e70daa595b2801aa6c1
Dark-Wap <168370710+Dark-Wap@users.noreply.github.com>
2025-02-04 10:47:58 -0600

    Update checkuser.py

--- checkuser.py
+++ checkuser.py
@@ -1,11 +1,9 @@
-from crypt import methods
+from flask import Flask, jsonify, request
 import os
 import sys
 import typing as t
 import json
-
 from datetime import datetime
-from flask import Flask, jsonify, request
 
 a = "{"
 b = "}"
@@ -17,54 +15,60 @@ app.config['JSON_SORT_KEYS'] = False
 def get_user(username: str) -> t.Optional[str]:
     command = 'userscheck %s 1' % username
     result = os.popen(command).readlines()
-    final = result[0].strip()
-    return final
+    if result:
+        final = result[0].strip()
+        return final
+    return None
 
 def cont_online(username: str) -> t.Optional[str]:
     command = 'userscheck %s 2' % username
     result = os.popen(command).readlines()
-    final = result[0].strip()
-    return final
+    if result:
+        final = result[0].strip()
+        return final
+    return None
 
 def limiter_user(username: str) -> t.Optional[str]:
     command = 'userscheck %s 3' % username
     result = os.popen(command).readlines()
-    final = result[0].strip()
-    return final
+    if result:
+        final = result[0].strip()
+        return final
+    return None
 
 def check_data(username: str) -> t.Optional[str]:
     command = 'userscheck %s 4' % username
     result = os.popen(command).readlines()
-    final = result[0].strip()
-    return final
+    if result:
+        final = result[0].strip()
+        return final
+    return None
 
 def check_dias(username: str) -> t.Optional[str]:
     command = 'userscheck %s 5' % username
     result = os.popen(command).readlines()
-    final = result[0].strip()
-    return final
+    if result:
+        final = result[0].strip()
+        return final
+    return None
 
-
-@app.route('/checkUser',methods = ['POST','GET'])
+@app.route('/checkUser', methods=['POST', 'GET'])
 def check_user():
     if request.method == 'POST':
         try:
             req_data = request.get_json()
             user_get = req_data.get("user")
-            username = get_user(user_get)
-            user = get_user(username)
+            user = get_user(user_get)
             if user == "Not exist":
-                return ("{0}\"username\":\"{1}\",\"count_connection\":\"Null\",\"expiration_date\":\"Null\",\"expiration_days\":\"Null\",\"limiter_user\":\"Null\"{2}" .format(a, user, b))
+                return "{0}\"username\":\"{1}\",\"count_connection\":\"Null\",\"expiration_date\":\"Null\",\"expiration_days\":\"Null\",\"limiter_user\":\"Null\"{2}".format(a, user_get, b)
             else:
-                return ("{0}\"username\":\"{1}\",\"count_connection\":\"{2}\",\"expiration_date\":\"{3}\",\"expiration_days\":\"{4}\",\"limiter_user\":\"{5}\"{6}" .format(a, username, cont_online(username), check_data(username), check_dias(username), limiter_user(username), b))
+                return "{0}\"username\":\"{1}\",\"count_connection\":\"{2}\",\"expiration_date\":\"{3}\",\"expiration_days\":\"{4}\",\"limiter_user\":\"{5}\"{6}".format(
+                    a, user_get, cont_online(user), check_data(user), check_dias(user), limiter_user(user), b
+                )
         except Exception as e:
             return jsonify({'error': str(e)})
     else:
-        try:
-            return 'Cannot GET /checkUser'
-        except Exception as e:
-            return jsonify({'error': str(e)})
-
+        return 'Cannot GET /checkUser'
 
 if __name__ == '__main__':
     app.run(
